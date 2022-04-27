7ef94503e74c7bf396df80e9b994a13668c735a6
williamdampier <74879032+williamdampier@users.noreply.github.com>
2022-04-27 19:24:55 +1200

    github users mid-done

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,8 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
+import Axios from 'axios';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState('');
+
+  //get things from global context
+  const { requests, error, searchGitHubUser } = React.useContext(GithubContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user) {
+      //do something if user search not empty
+      searchGitHubUser(user);
+    }
+  };
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <MdSearch />
+            <input
+              type='text'
+              placeholder='enter github user'
+              value={user}
+              onChange={(e) => {
+                setUser(e.target.value);
+              }}
+            />
+            {requests > 0 && <button type='submit'>search</button>}
+          </div>
+        </form>
+        <h3>requests {requests} / 60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,74 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+//Provider, Consumer - GithubContext.Provider
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequest] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  //search user
+  const searchGitHubUser = async (user) => {
+    toggleError();
+    setLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setGithubUser(response.data);
+      console.log(response);
+    } else {
+      toggleError(true, 'there is no user with this username');
+    }
+    setLoading(false);
+  };
+  //check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequest(remaining);
+        if (remaining === 0) {
+          //throw error
+          toggleError(
+            true,
+            ' sorry, you have exceeded your hourly rate limit!'
+          );
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg });
+  }
+
+  useEffect(checkRequests, []);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        toggleError,
+        error,
+        searchGitHubUser,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export { GithubContext, GithubProvider };
